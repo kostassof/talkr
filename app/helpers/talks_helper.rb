5f96493de002ecbf4353a4a3fc70c9e44f159498
Kostass <kostassof88@gmail.com>
2017-11-27 23:09:43 +0100

    Add AvatarHelper.

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,10 +1,4 @@
 module TalksHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-    image_tag gravatar_url
-  end
-
   def like_button(talk)
     if current_user.liked?(talk)
       link_to "Unlike", unlike_talk_path(talk), method: :delete
