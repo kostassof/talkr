b2c3060c1199b6fbf755bb9d13692f339df75283
Kostass <kostassof88@gmail.com>
2018-01-28 02:29:14 +0100

    Followers relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,17 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_talks, through: :likes, source: :talk
 
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+  has_many :followed_user_relationships,
+    foreign_key: :follower_id,
+    class_name: "FollowingRelationship",
+    dependent: :destroy
+  has_many :followed_users, through: :followed_user_relationships
+
+  has_many :follower_relationships,
+    foreign_key: :followed_user_id,
+    class_name: "FollowingRelationship",
+    dependent: :destroy
+  has_many :followers, through: :following_relationships
 
   def like(talk)
     liked_talks << talk
