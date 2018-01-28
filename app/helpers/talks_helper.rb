2d15b166699f423c03f29312365eb39a4d2f929f
Kostass <kostassof88@gmail.com>
2018-01-28 17:43:49 +0100

    Remove hidden field from helper method.

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,7 +1,6 @@
 module TalksHelper
   def talk_form_for(content_type)
     form_for(Talk.new, url: content_type.new) do |form|
-      form.hidden_field(:content_type, value: content_type) +
       form.fields_for(:content) { |content_form| yield(content_form) } +
       form.submit("Talk.")
     end
