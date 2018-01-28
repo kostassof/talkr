33e15434eac8dd47f2b64390f66837ff5fc03f2f
Kostass <kostassof88@gmail.com>
2018-01-28 17:24:36 +0100

    Form helper method.

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,4 +1,12 @@
 module TalksHelper
+  def talk_form_for(content_type)
+    form_for Talk.new do |form|
+      form.hidden_field(:content_type, value: content_type) +
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit("Talk.")
+    end
+  end
+
   def like_button(talk)
     if current_user.liked?(talk)
       link_to "Unlike", unlike_talk_path(talk), method: :delete
